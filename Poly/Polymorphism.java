5e1b2b1d639804e64c2cd7c83c080e9a171c5121
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-04-12 12:20:18 -0400

    overloading examples

--- Poly/Polymorphism.java
+++ Poly/Polymorphism.java
@@ -4,9 +4,19 @@ class Bird {
   public void sing(){
     System.out.println("tweet");
   }
+  // public void sing(int num){
+  //   for(int i = 0; i < num; i++){
+  //     System.out.println(i + " robin tweet");
+  //   }
+  // }
 }
 
 class Robin extends Bird {
+  public void sing(int num){
+    for(int i = 0; i < num; i++){
+      System.out.println(i + " robin tweet");
+    }
+  }
   public void sing(){
     System.out.println("robin tweet");
   }
@@ -16,14 +26,21 @@ class Pelican extends Bird {
   public void sing(){
     System.out.println("pelican tweet");
   }
+  public void sing(int num){
+    for(int i = 0; i < num; i++){
+      System.out.println(i + " pelican tweet");
+    }
+  }
 }
 
 public class Polymorphism {
   public static void main(String[] args){
-    Bird b = new Bird();
-    b.sing();
+    Pelican b = new Pelican();
+    b.sing(3);
   }
 }
 
 
-//polymorphism is the concept of overwriting a class method with a subclass method
+//polymorphism
+//overloading: same method name, but different parameters 
+//overwriting: is the concept of overwriting a class method with a subclass method
